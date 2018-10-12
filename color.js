19475009f4ab96c5ca0c28b8e2d4134e9b5db176
Blake Joynes <blakejoynes@gmail.com>
2018-10-12 13:09:58 -0400

    updated css

--- color.js
+++ color.js
@@ -1,4 +1,6 @@
-var colors = generateRandomColors(6);
+var numOfSquares = 6;
+
+var colors = generateRandomColors(numOfSquares);
 
 
 var squares = document.getElementsByClassName("square");
@@ -7,8 +9,45 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
 var h1 = document.querySelector("h1");
 var resetButton = document.getElementById("reset");
-
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 colorDisplay.textContent = pickedColor;
+
+
+easyBtn.addEventListener("click", function(){
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    numOfSquares = 3;
+    colors = generateRandomColors(numOfSquares);
+
+    pickedColor = pickColor();
+
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+        if (colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        }else{
+            squares[i].style.display = "none";
+        }
+    }
+    h1.style.backgroundColor = "steelblue";
+
+});
+
+hardBtn.addEventListener("click", function(){
+    easyBtn.classList.remove("selected");
+    hardBtn.classList.add("selected");
+    numOfSquares = 6
+    colors = generateRandomColors(numOfSquares);
+
+    pickedColor = pickColor();
+
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+});
 
 for (var i=0; i < squares.length; i++){
     //add init colors to squares
@@ -31,17 +70,18 @@ for (var i=0; i < squares.length; i++){
 }
 
 resetButton.addEventListener("click", function(){
-    resetButton.textContent = "New Color";
-    colors = generateRandomColors(6);
+    resetButton.textContent = "New Colors";
+    colors = generateRandomColors(numOfSquares);
 
     pickedColor = pickColor();
 
-    colorDisplay.textContent = pickedColor
+    colorDisplay.textContent = pickedColor;
+    messageDisplay.textContent = "";
 
     for(var i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = colors[i];
     }
-    h1.style.backgroundColor = "#232323";
+    h1.style.backgroundColor = "steelblue";
 });
 
 
